1987dfa18a3b04ce56ca8b1514dffe2556bffd92
wassen <k-asayama@outlook.com>
2024-01-29 00:26:02 +0900

    nvim 関連ファイル

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -1,24 +1,26 @@
 vim.g.mapleader = " "
 
-vim.api.nvim_set_keymap('n', ';', ':', {noremap = true})
-vim.api.nvim_set_keymap('n', ':', ';', {noremap = true})
-vim.api.nvim_set_keymap('v', ';', ':', {noremap = true})
-vim.api.nvim_set_keymap('v', ':', ';', {noremap = true})
-vim.api.nvim_set_keymap('v', ':', ';', {noremap = true})
+vim.api.nvim_set_keymap('n', ';', ':', { noremap = true })
+vim.api.nvim_set_keymap('n', ':', ';', { noremap = true })
+vim.api.nvim_set_keymap('v', ';', ':', { noremap = true })
+vim.api.nvim_set_keymap('v', ':', ';', { noremap = true })
+vim.api.nvim_set_keymap('v', ':', ';', { noremap = true })
 
-vim.api.nvim_set_keymap('', '<S-h>', '^', {noremap = false})
-vim.api.nvim_set_keymap('', '<S-l>', '$', {noremap = false})
+vim.api.nvim_set_keymap('', '<S-h>', '^', { noremap = false })
+vim.api.nvim_set_keymap('', '<S-l>', '$', { noremap = false })
 
-vim.api.nvim_set_keymap('i', 'jj', '<C-[>', {noremap = true})
+vim.api.nvim_set_keymap('i', 'jj', '<C-[>', { noremap = true })
 
 -- lsp
 -- lsp用のBindingはlspの設定に置くべきか
 -- lsp対応かどうかの判別？
-vim.api.nvim_set_keymap('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true })
 -- これとの違いはなんだ
 -- vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.api.nvim_set_keymap('n', '<Leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>lr', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>l<Leader>', '<cmd>Telescope<CR>', { noremap = true })
 
 -- buffer
-vim.api.nvim_set_keymap('n', '<Leader>bh', '<cmd>bprevious<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>bl', '<cmd>bnext<CR>', {noremap = true})
-
+vim.api.nvim_set_keymap('n', '<Leader>bh', '<cmd>bprevious<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>bl', '<cmd>bnext<CR>', { noremap = true })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -11,6 +11,11 @@ require("packer").startup(function(use)
 	use 'neovim/nvim-lspconfig'
 	use 'williamboman/mason.nvim'
 	use 'williamboman/mason-lspconfig.nvim'
+
+	-- bufferlineの選択状態が分かりづらいもんだい
+	-- smartcase
+	-- Escで検索ハイライトの削除
+	-- omni funcでscreenが開かれるのがうざい。Telescopeで出してくれ
 	use { 'akinsho/nvim-bufferline.lua', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
 	-- require nerd font. see https://github.com/ryanoasis/nerd-fonts#patched-fonts
 	use { 'lukas-reineke/indent-blankline.nvim' }
@@ -21,9 +26,23 @@ require("packer").startup(function(use)
 	use { "ellisonleao/gruvbox.nvim", opt = true }
 	use { 'folke/tokyonight.nvim', opt = true }
 
-	use { 'ibhagwan/fzf-lua',
+	use {
+		'ibhagwan/fzf-lua',
 		-- optional for icon support
 		requires = { 'kyazdani42/nvim-web-devicons' }
 	}
 	use { 'lambdalisue/fern.vim' }
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = {
+			{ 'nvim-lua/plenary.nvim' },
+		},
+	}
+
+	use { 'azabiong/vim-highlighter' }
+	use { 'nvim-telescope/telescope-ui-select.nvim' }
+	use {
+		'dart-lang/dart-vim-plugin',
+		ft = 'dart'
+	}
 end)
